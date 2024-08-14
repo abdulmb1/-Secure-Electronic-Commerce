0acfa24a652609f15f1f77442edfd7d2d5cf58a2
Unsxlved <s3947427@student.rmit.edu.au>
2024-08-14 16:50:55 +1000

    q3 draft code

---
+++ q3/script3.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const togglePassword = document.querySelector('.pw_hide');
+    const passwordInput = document.querySelector('.input_box input[type="password"]');
+
+    togglePassword.addEventListener('click', () => {
+        // Toggle the type attribute using a ternary operator
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+
+        // Toggle the eye/eye-slash icon
+        togglePassword.classList.toggle('uil-eye-slash');
+        togglePassword.classList.toggle('uil-eye');
+    });
+});
+
+
